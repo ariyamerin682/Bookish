63b251761590765dfba1ae7eb5d0c45836d82afa
ariyamerin682 <ariyamerin682@users.noreply.github.com>
2026-07-19 20:54:23 +0530

    fix API URL for production

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:5000/api';
+const API = '/api';
 const GBOOKS_KEY = 'AIzaSyAeCsiDbyMrsTfgPOfHYHYfbFygQWsPvfg';
 
 let token = localStorage.getItem('bookish_token') || null;
